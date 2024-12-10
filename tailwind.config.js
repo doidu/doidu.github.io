cc729711d74d8318e7bb4c95ae65bb7ca57fd0cb
doidu <151105663+doidu@users.noreply.github.com>
2024-12-10 13:56:58 +0000

    added tailwind framework

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}npx tailwindcss -i ./src/input.css -o ./src/output.css --watch"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

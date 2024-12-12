fedbaaa2bb558b21f9c38f858060332960d8ffd9
doidu <151105663+doidu@users.noreply.github.com>
2024-12-12 17:26:36 +0000

    v0.2222

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/.{html,js}"],
+  content: {
+    files: ["./src/*.{html,js}"],
+  },
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
